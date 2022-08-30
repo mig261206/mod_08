cc09e77b3bc15f83b857d749deec917634d70aba
Carlos <keicarlos62@gmail.com>
2022-08-30 09:21:28 -0300

    Initial commit

---
+++ src/nota.java
@@ -0,0 +1,15 @@
+public class Operadores {
+
+    public static void maim(String args[] ) {
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = num1 + num2;
+        System.out.println(num3);
+
+
+
+    }
+
+
+}
+
